e0a18edd308a00f46c0631bd3433af4d53331bb2
Michael Barz <mb@barz.de>
2022-04-21 17:25:30 +0200

    added time offset and delay compensation

--- msp_pupil_labs/pupil_remote.py
+++ msp_pupil_labs/pupil_remote.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 import msgpack
 import zmq
 from PIL import Image
+import time
 
 
 class GazeSample:
@@ -114,6 +115,7 @@ class PupilRemote:
 
         self._start_required_plugins()
         self._start_subscription()
+        self.sync_timestamp()
 
     def close(self):
         self.subscriber.close()
@@ -147,11 +149,10 @@ class PupilRemote:
             if len(payload) > 1:
                 message["raw_img"] = payload[1:]  # e.g., the encoded image of the scene camera
         except Exception as e:
-            if e.errno == 11:  # Resource is temporarily not available
+            if e.errno == 11 or 36:  # Resource is temporarily not available
                 return None, None
             else:
                 raise e
-
         return topic, message
 
     @staticmethod
@@ -180,6 +181,7 @@ class PupilRemote:
                 if self.video_resolution is None:
                     self.video_resolution = image.size
                 message["image"] = image
+                self.correct_timestamp(message)
                 return topic, message
 
             elif topic.startswith(PupilRemote.Streams.GAZE) and self.video_resolution is not None:
@@ -192,6 +194,7 @@ class PupilRemote:
                     origin=GazeSample.ORIGIN_BOTTOM_LEFT
                 )
                 message["gaze"] = gaze_sample
+                self.correct_timestamp(message)
                 return topic, message
 
             elif topic.startswith(PupilRemote.Streams.FIXATIONS) and self.video_resolution is not None:
@@ -208,6 +211,77 @@ class PupilRemote:
                     origin=GazeSample.ORIGIN_BOTTOM_LEFT
                 )
                 message["fixation"] = gaze_sample
+                self.correct_timestamp(message)
                 return topic, message
 
         return None, None
+
+    def correct_timestamp(self, message):
+        message["timestamp"] = message["timestamp"] - self.pupil_time_offset
+
+    def sync_timestamp(self):
+        # set current Pupil time to timestamp
+        local_clock = time.perf_counter
+        offset = self.measure_clock_offset(clock_function=local_clock)
+        print(f"Clock offset (1 measurement): {offset} seconds")
+        number_of_measurements = 10
+        stable_offset_mean = self.measure_clock_offset_stable(
+            clock_function=local_clock, nsamples=number_of_measurements
+        )
+        print(
+            f"Mean clock offset ({number_of_measurements} measurements): "
+            f"{stable_offset_mean} seconds"
+        )
+
+        # 5. Infer pupil clock time from "local" clock measurement
+        local_time = local_clock()
+        pupil_time_calculated_locally = local_time + stable_offset_mean
+        print(f"Local time: {local_time}")
+        print(f"Pupil time (calculated locally): {pupil_time_calculated_locally}")
+        self.pupil_time_offset = stable_offset_mean
+
+    def request_pupil_time(self):
+        """Uses an existing Pupil Core software connection to request the remote time.
+        Returns the current "pupil time" at the timepoint of reception.
+        See https://docs.pupil-labs.com/core/terminology/#pupil-time for more information
+        about "pupil time".
+        """
+        self.pupil_remote.send_string("t")
+        pupil_time = self.pupil_remote.recv()
+        return float(pupil_time)
+
+    def measure_clock_offset(self, clock_function):
+        """Calculates the offset between the Pupil Core software clock and a local clock.
+        Requesting the remote pupil time takes time. This delay needs to be considered
+        when calculating the clock offset. We measure the local time before (A) and
+        after (B) the request and assume that the remote pupil time was measured at (A+B)/2,
+        i.e. the midpoint between A and B.
+        As a result, we have two measurements from two different clocks that were taken
+        assumingly at the same point in time. The difference between them ("clock offset")
+        allows us, given a new local clock measurement, to infer the corresponding time on
+        the remote clock.
+        """
+        local_time_before = clock_function()
+        pupil_time = self.request_pupil_time()
+        local_time_after = clock_function()
+
+        local_time = (local_time_before + local_time_after) / 2.0
+        clock_offset = pupil_time - local_time
+        return clock_offset
+
+    def measure_clock_offset_stable(self, clock_function, nsamples=10):
+        """Returns the mean clock offset after multiple measurements to reduce the effect
+        of varying network delay.
+        Since the network connection to Pupil Capture/Service is not necessarily stable,
+        one has to assume that the delays to send and receive commands are not symmetrical
+        and might vary. To reduce the possible clock-offset estimation error, this function
+        repeats the measurement multiple times and returns the mean clock offset.
+        The variance of these measurements is expected to be higher for remote connections
+        (two different computers) than for local connections (script and Core software
+        running on the same computer). You can easily extend this function to perform
+        further statistical analysis on your clock-offset measurements to examine the
+        accuracy of the time sync.
+        """
+        assert nsamples > 0, "Requires at least one sample"
+        offsets = [self.measure_clock_offset(clock_function) for x in range(nsamples)]
+        return sum(offsets) / len(offsets)  # mean offset
