05b096d4a82206fee672a953f5126e55bcba9b3c
Michael Barz <mb@barz.de>
2022-04-27 18:08:22 +0200

    reversed gaze stream filter, had no effect

--- msp_pupil_labs/pupil_remote.py
+++ msp_pupil_labs/pupil_remote.py
@@ -117,7 +117,7 @@ class Fixation:
 
 class PupilRemote:
     class Streams:
-        GAZE = "gaze.3d.01."
+        GAZE = "gaze."
         FIXATIONS = "fixations"
         SURFACES = "surfaces"
         SCENE_VIDEO = "frame.world"
