28de160da7f98a748a26969636b9dc8fffccf00e
ombh01 <ombh01@dfki.de>
2022-04-28 16:56:40 +0200

    Update pupil_remote.py

--- msp_pupil_labs/pupil_remote.py
+++ msp_pupil_labs/pupil_remote.py
@@ -5,114 +5,7 @@ import numpy as np
 import zmq
 from PIL import Image
 import time
-
-
-class GazeSample:
-    """
-    Data structure for gaze data. It enforces
-    (1) that the origin of gaze coordinates is at the upper left, and
-    (2) the gaze coordinates are normalized.
-    """
-
-    # see also https://pillow.readthedocs.io/en/stable/handbook/concepts.html#coordinate-system
-    ORIGIN_BOTTOM_LEFT = "bl"
-    ORIGIN_TOP_LEFT = "tl"
-
-    def __init__(self, gaze, reference_size, normalized=True, origin="bl"):
-        self._gaze = self._normalize(gaze, normalized, origin)
-        self._max_width = reference_size[0]
-        self._max_height = reference_size[1]
-
-    def _normalize(self, gaze, normalized, origin):
-        x, y = tuple(gaze)
-        if not normalized:
-            x /= float(self.max_width)
-            y /= float(self.max_height)
-
-        if origin == self.ORIGIN_BOTTOM_LEFT:
-            # convert to top-left coordinate
-            y = 1. - y
-
-        return x, y
-
-    @property
-    def x(self):
-        return self.gaze[0]
-
-    @property
-    def y(self):
-        return self.gaze[1]
-
-    @property
-    def gaze(self):
-        return self._gaze
-
-    @property
-    def x_scaled(self):
-        return self.x * self.max_width
-
-    @property
-    def y_scaled(self):
-        return self.y * self.max_height
-
-    @property
-    def gaze_scaled(self):
-        return self.x_scaled, self.y_scaled
-
-    @property
-    def max_width(self):
-        return self._max_width
-
-    @property
-    def max_height(self):
-        return self._max_height
-
-
-class Fixation:
-
-    def __init__(self, fixation_id: int, timestamp: float, duration: float, norm_pos: np.ndarray, video_resolution):
-        self._is_complete = False
-        self._fixation_id = fixation_id
-        self._timestamps = []
-        self._durations = []
-        self._norm_pos = []
-        self._video_resolution = video_resolution
-        self.add_partial_fixation(fixation_id, timestamp, duration, norm_pos)
-
-    def add_partial_fixation(self, fixation_id: int, timestamp: float, duration: float, norm_pos: np.ndarray):
-        assert self._fixation_id == fixation_id
-        self._timestamps.append(timestamp)
-        self._norm_pos.append(norm_pos)
-        self._durations.append(duration)
-
-    def finalize(self) -> 'Fixation':
-        self._is_complete = True
-        return self
-        
-    @property
-    def index(self):
-        return self._fixation_id
-
-    @property
-    def timestamp(self) -> float:
-        return self._timestamps[0]
-
-    @property
-    def duration(self):
-        if self._is_complete:
-            return (self._timestamps[-1] - self._timestamps[0]) * 1000. + self._durations[-1]
-        return None
-    
-    @property
-    def fixation_point(self):
-        norm_positions = np.vstack(self._norm_pos)
-        fixation = GazeSample(
-            gaze=norm_positions.mean(axis=0),
-            normalized=True,
-            reference_size=self._video_resolution,
-            origin=GazeSample.ORIGIN_BOTTOM_LEFT
-        )
-        return fixation
+from  .util import decode_image, Fixation, GazeSample
 
 
 class PupilRemote:
@@ -204,20 +97,6 @@ class PupilRemote:
                 raise e
         return topic, message
 
-    @staticmethod
-    def _decode_image(raw_img, w, h, format="bgr") -> Image.Image:
-        if format == "bgr":
-            img = Image.frombytes(mode="RGB", size=(w, h), data=raw_img)
-            b, g, r = img.split()
-            img = Image.merge("RGB", (r, g, b))
-            return img
-        elif format == "jpeg":
-            img = Image.open(io.BytesIO(raw_img))
-            img.load()  # is required, because Pillow uses lazy loading which causes problems later on
-            return img
-        else:
-            raise NotImplementedError(f"image format {format} is not supported.")
-
     def _handle_partial_fixations(self, message) -> Optional[Fixation]:
         fixation_id = message["id"]
         timestamp = message["timestamp"]
@@ -260,7 +139,7 @@ class PupilRemote:
                 encoded_image = message["raw_img"][0]
                 if encoded_image is None:
                     return
-                image = self._decode_image(encoded_image, message["width"], message["height"], format=message["format"])
+                image = decode_image(encoded_image, message["width"], message["height"], format=message["format"])
                 self.video_resolution = image.size
                 message["image"] = image
                 self.correct_timestamp(message)
